9eb84e5d90b245538d5adf0854b9b27cea394be7
Daniel Olguín Martínez <dani.olg@hotmail.com>
2023-08-14 01:19:00 -0400

    feat: añadir tipo auto

---
+++ src/vehiculos/Auto.java
@@ -0,0 +1,39 @@
+package vehiculos;
+
+public class Auto extends Vehiculo {
+    private int codigo;
+    private String marca;
+    private String modelo;
+    private int fabricado;
+    private int kilometraje;
+    private String patente;
+
+    public Auto(String marca, String modelo, int fabricado, int kilometraje, String patente) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.fabricado = fabricado;
+        this.kilometraje = kilometraje;
+        this.patente = patente;
+    }
+
+    @Override
+    public String toString() {
+        return "- Codigo: " + this.codigo+
+                "\n- Marca: " + this.marca +
+                "\n- Tipo: Auto" +
+                "\n- Modelo: " + this.modelo +
+                "\n- Año: " + this.fabricado +
+                "\n- Kilometraje: " + this.kilometraje +
+                "\n- Patente: " + this.patente;
+    }
+
+    @Override
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public int getCodigo() {
+        return this.codigo;
+    }
+}
