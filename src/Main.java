1a0c5204bc09ba492c0aad2f8a0401c71c21b0bd
Daniel Olguín Martínez <dani.olg@hotmail.com>
2023-08-14 01:19:41 -0400

    feat: pruebas del gestor

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import gestion.GestorGeneral;
+import vehiculos.Camioneta;
+import vehiculos.Motocicleta;
+
 public class Main {
     public static void main(String[] args) {
+        Camioneta c = new Camioneta("MAZDA", "CX3", 2020, 231421, "SH23IJ");
+        Motocicleta m = new Motocicleta("YAMAHA", "YTZ", 2023, 9200, "KF123");
 
+        Motocicleta modif = new Motocicleta("YAMAHA", "YTZ", 2023, 9200, "KF321");
+
+        GestorGeneral gestorGeneral = new GestorGeneral();
+        gestorGeneral.iniciarPrograma();
     }
 }
